1fafa169ebe36b777576468d601ba80718fa7b31
Zoltan Altfatter <altfatterz@gmail.com>
2015-07-14 10:09:28 +0200

    inital commit

---
+++ src/main/java/com/altfatterz/SpringBootNonJdbcDatabaseUrlApplication.java
@@ -0,0 +1,12 @@
+package com.altfatterz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNonJdbcDatabaseUrlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootNonJdbcDatabaseUrlApplication.class, args);
+    }
+}
