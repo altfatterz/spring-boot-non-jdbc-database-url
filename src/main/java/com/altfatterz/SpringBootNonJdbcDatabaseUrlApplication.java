6b7cbf03264f90cb12c1f05b19840362f5241860
Zoltan Altfatter <altfatterz@gmail.com>
2015-07-14 21:38:53 +0200

    initial configuration

--- src/main/java/com/altfatterz/SpringBootNonJdbcDatabaseUrlApplication.java
+++ src/main/java/com/altfatterz/SpringBootNonJdbcDatabaseUrlApplication.java
@@ -1,10 +1,26 @@
 package com.altfatterz;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @SpringBootApplication
+@RestController
 public class SpringBootNonJdbcDatabaseUrlApplication {
+
+    @Autowired
+    DataSource datasource;
+
+    @RequestMapping("/")
+    public String greeting() throws SQLException {
+        return datasource.getClass().getName();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootNonJdbcDatabaseUrlApplication.class, args);
